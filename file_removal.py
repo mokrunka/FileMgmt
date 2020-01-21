04b048f3bd45f5b52f0beadef00407019cd2d6e4
mokrunka <hunker.engineer@gmail.com>
2020-01-20 18:46:34 -0800

    initial commit for a tool to remove files older than a certain date

--- file_removal.py
+++ file_removal.py
@@ -1,1 +1,22 @@
-#test
+import os
+from pathlib import Path
+
+pathName = input("Type in the absolute pathname of the directory you want to remove: ")
+dir_to_remove = Path(pathName)
+
+#empty list of the timestamps each of the files in dir_to_remove
+list_of_timestamps = []
+
+with os.scandir(dir_to_remove) as dir_contents:
+    for file in dir_contents:
+        last_modified = file.stat()
+        if last_modified.st_mtime <= 1579574184:
+            list_of_timestamps.append(file)
+
+#declare an empty filename to place-hold
+full_file_name = ''
+
+#this is going to remove some files
+for file in list_of_timestamps:
+    full_file_name = dir_to_remove / file
+    os.remove(full_file_name)
