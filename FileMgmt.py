3932d5dc9851f27c6c966bdd98aae216db1b0586
mokrunka <hunker.engineer@gmail.com>
2020-05-20 11:29:45 -0700

    added comment to FileMgmt.py

--- FileMgmt.py
+++ FileMgmt.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+# store the pathname for the dir to be removed
 pathName = input("Type in the absolute pathname of the directory you want to remove: ")
 dir_to_remove = Path(pathName)
 
